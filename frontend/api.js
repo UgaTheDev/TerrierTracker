b7b79eaa20fd7b9ad5292c54f0cb70cc20b2c2dd
UgaTheDev <kush.zingade@gmail.com>
2025-09-29 23:14:20 -0400

    Link to railway

--- frontend/api.js
+++ frontend/api.js
@@ -1,5 +1,4 @@
-const API_BASE_URL = "http://localhost:5000/api";
-
+const API_BASE_URL = "https://terriertracker-production.up.railway.app/api";
 const apiRequest = async (endpoint, options = {}) => {
   try {
     const response = await fetch(`${API_BASE_URL}${endpoint}`, {
